350a730e53f6fe370c6b2fdb4680c51ed7bed62a
Sergei <woronowserg@gmail.com>
2020-09-20 15:00:45 +0000

    server.js fix

--- server.js
+++ server.js
@@ -9,4 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 3001);
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 80, '0.0.0.0', () => {
+  console.log("Server started.");
+});
